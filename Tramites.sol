a5398c43b8264bf2873b312b6d74acd470486b6a
BeeEpisodios <jhonq2512@gmail.com>
2025-05-15 12:57:55 -0500

    mejorando el contranto 1.0

--- Tramites.sol
+++ Tramites.sol
@@ -3,8 +3,8 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract Tramites {
-    uint tramiteCounter;
-    uint MovimientoCounter;
+    uint private tramiteCounter;
+    uint private MovimientoCounter;
 
     struct Tramite{
         address creador;
@@ -19,8 +19,14 @@ contract Tramites {
         uint[] documentos;
     }
 
-    mapping (uint => Tramite) allTramites;
-    mapping (uint => Movimiento) allMovimientos;
+    event tramiteCreado(uint idTramite, address creador, uint tipoTramite);
+    event movimientoCreado(uint idTramite, uint idMovimiento, address emisor, address receptor);
+    event movimientoEliminado(uint idTramite, uint idMovimiento);
+
+    mapping (address => bool) private autorizacion;
+    mapping (address => uint[]) private userTramites;
+    mapping (uint => Tramite) private allTramites;
+    mapping (uint => Movimiento) private allMovimientos;
 
     modifier onlyEmisor(uint _idTramite) {
         uint longitudS = allTramites[_idTramite].seguimiento.length;
@@ -30,33 +36,86 @@ contract Tramites {
         _;
     }
 
-    function crearTramite(uint _tipoTramite, address _receptor, uint[] calldata _documentos) external {
+    modifier onlyReceptor(uint _idTramite) {
+        uint longitudS = allTramites[_idTramite].seguimiento.length;
+        if(longitudS == 0) {
+            _;
+        } else {
+        uint idUltimoMovimiento = allTramites[_idTramite].seguimiento[longitudS - 1];
+        require(msg.sender == allMovimientos[idUltimoMovimiento].receptor, "No eres el receptor");
+        _;
+        }
+    }
+
+    // ============================ TRAMITES =============================
+    //funcion para iniciar un nuevo trámite y registrar el primer movimiento
+    function crearTramite(uint _tipoTramite, address _receptor, uint[] calldata _documentos) 
+    external
+    {
+        userTramites[msg.sender].push(tramiteCounter);  
         allTramites[tramiteCounter] = Tramite(msg.sender, _tipoTramite, new uint[](0));
+        emit tramiteCreado(tramiteCounter, msg.sender, _tipoTramite);
         crearMovimiento(tramiteCounter, _receptor, _documentos);
         tramiteCounter ++;
     }
 
-    function crearMovimiento(uint _idTramite, address _receptor, uint[] calldata _documentos) public {
+    //funcion para añadir movimientos al historial de un tramite si el receptor es al que fue digirido.
+    function crearMovimiento(uint _idTramite, address _receptor, uint[] calldata _documentos) 
+    public
+    onlyReceptor(_idTramite) 
+    {
         allMovimientos[MovimientoCounter] = Movimiento(msg.sender, _receptor, block.timestamp, _documentos);
         allTramites[_idTramite].seguimiento.push(MovimientoCounter);
+        emit movimientoCreado(_idTramite, MovimientoCounter, msg.sender, _receptor);
         MovimientoCounter ++;
     }
 
-    function quitarMovimiento(uint _idTramite) external onlyEmisor(_idTramite) {
+    //quitar el ultimo movimiento pero solo si fue creado por el mismo que llama la funcion.
+    function quitarMovimiento(uint _idTramite) 
+    external
+    onlyEmisor(_idTramite) 
+    {
+        uint idEliminado = allTramites[_idTramite].seguimiento[allTramites[_idTramite].seguimiento.length - 1];
+        emit movimientoEliminado(_idTramite, idEliminado);
         allTramites[_idTramite].seguimiento.pop();
     }
 
-    //funciones para consumo de datos
+    // =================== CONSUMO DE DATOS ==================
 
+    //Obtener una estructura tramite por su id
     function getTramite(uint _idTramite) external view returns (Tramite memory) {
         return allTramites[_idTramite];
     }
 
+    //obtener una estructura movimiento por su id
     function getMovimiento(uint _idMovimiento) external view returns (Movimiento memory) {
         return allMovimientos[_idMovimiento];
     }
 
-    function getLength(uint _idTramite) external view returns (uint) {
-        return allTramites[_idTramite].seguimiento.length;
+    //Obtener la lista de Tramites creados por un usuario
+    function getUserTramites(address _sender) external view returns (Tramite[] memory) {
+        uint[] memory idTramites = userTramites[_sender];
+        Tramite[] memory tramites = new Tramite[](idTramites.length);
+        for (uint i = 0; i < idTramites.length; i++) 
+        {
+            tramites[i] = allTramites[idTramites[i]];
+        }
+        return tramites;
     }
+
+    //Obtener el historial en orden de un determiando tramite
+    function getHistorial(uint _idTramite) external view returns (Movimiento[] memory) {
+        uint[] memory seguimiento = allTramites[_idTramite].seguimiento;
+        Movimiento[] memory historial = new Movimiento[](seguimiento.length);
+
+        for (uint i = 0; i < seguimiento.length; i++) {
+            historial[i] = allMovimientos[seguimiento[i]];
+        }
+        return historial;
+    }
+
+    /*
+    -SERIA BUENO AGREGAR AUTORIZACION PARA QUE SOLO DETERMINADAS DIRECCIONES PUEDAN LLAMAR LAS FUNCIONES
+    -SE PUEDE AGREGAR UN RELAYER PARA QUE SE DELEGUE TODOS LOS CONSTOS DE TRANSACCION A UNA DIRECCION
+    */
 }
